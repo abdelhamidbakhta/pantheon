eb52fb6f0dd37727c3140158832bcb1f10b1db3d
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-09-09 09:58:51 +0200

    spotless apply

--- ethereum/jsonrpc/src/test/java/tech/pegasys/pantheon/ethereum/jsonrpc/TraceJsonRpcHttpBySpecTest.java
+++ ethereum/jsonrpc/src/test/java/tech/pegasys/pantheon/ethereum/jsonrpc/TraceJsonRpcHttpBySpecTest.java
@@ -23,8 +23,6 @@ import tech.pegasys.pantheon.ethereum.jsonrpc.internal.queries.BlockchainQueries
 import java.net.URL;
 import java.util.Map;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
